31a4f155c222911456b25a58620f285732d64037
Bruno St John <121534527+brunostjohn@users.noreply.github.com>
2025-05-31 23:32:38 +0100

    mmmm jiggle

---
+++ readme.md
@@ -0,0 +1,16 @@
+![Tweet](https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/a208405937bbc20625c1bf357988f14f419538ecf6900b2a945bb9628691efd7_1.jpg)
+
+# Studio Jiggly
+
+A shitty mouse jiggler for boring corporate jobs.
+
+## What it does
+
+- Pretends to be a totally legit Lenovo mouse
+- Jiggles that shit
+- I literally have no idea what I'm supposed to say here, it's a mouse jiggler. You get it.
+
+## How to use
+
+- Don't ask me, if you know how to build it, build it. If you don't, don't.
+- And also, don't ask me how to use it.
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,37 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
+#![allow(static_mut_refs)]
 
-use bsp::entry;
-use defmt::*;
-use defmt_rtt as _;
+use embedded_hal::digital::InputPin;
 use embedded_hal::digital::OutputPin;
-use panic_probe as _;
+use fugit::ExtU32;
+use panic_halt as _;
+use rp_pico::entry;
+use rp_pico::hal;
+use rp_pico::hal::gpio;
+use rp_pico::hal::pac;
+use rp_pico::hal::pac::interrupt;
+use rp_pico::hal::prelude::*;
+use rp_pico::hal::timer::Alarm;
+use rp_pico::hal::timer::Instant;
+use rp_pico::hal::Timer;
+use usb_device::{class_prelude::*, prelude::*};
+use usbd_hid::descriptor::generator_prelude::*;
+use usbd_hid::descriptor::MouseReport;
+use usbd_hid::hid_class::HIDClass;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
-
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+static mut USB_HID: Option<HIDClass<hal::usb::UsbBus>> = None;
 
 #[entry]
 fn main() -> ! {
-    info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -44,34 +42,128 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    #[cfg(feature = "rp2040-e5")]
+    {
+        let sio = hal::Sio::new(pac.SIO);
+        let _pins = rp_pico::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+    }
+    let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
-    let pins = bsp::Pins::new(
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+    unsafe {
+        USB_BUS = Some(usb_bus);
+    }
+
+    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
+
+    let usb_hid = HIDClass::new(bus_ref, MouseReport::desc(), 60);
+    unsafe {
+        USB_HID = Some(usb_hid);
+    }
+
+    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x17ef, 0x6019))
+        .max_power(100)
+        .unwrap()
+        .usb_rev(usb_device::device::UsbRev::Usb200)
+        .device_release(0x6300)
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Logitech")
+            .product("Lenovo USB Optical Mouse")])
+        .unwrap()
+        .device_class(0)
+        .build();
+    unsafe {
+        USB_DEVICE = Some(usb_dev);
+    }
+
+    unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
+
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
     let mut led_pin = pins.led.into_push_pull_output();
 
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
+        let _ = led_pin.set_high();
+
+        let rep_left = MouseReport {
+            x: -5,
+            y: 0,
+            buttons: 0,
+            wheel: 0,
+            pan: 0,
+        };
+        push_mouse_movement(rep_left).ok().unwrap_or(0);
+
         delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
+
+        let rep_right = MouseReport {
+            x: 5,
+            y: 0,
+            buttons: 0,
+            wheel: 0,
+            pan: 0,
+        };
+        push_mouse_movement(rep_right).ok().unwrap_or(0);
+
+        delay.delay_ms(500);
+
+        let rep_up = MouseReport {
+            x: 0,
+            y: -5,
+            buttons: 0,
+            wheel: 0,
+            pan: 0,
+        };
+        push_mouse_movement(rep_up).ok().unwrap_or(0);
+
+        delay.delay_ms(500);
+
+        let rep_down = MouseReport {
+            x: 0,
+            y: 5,
+            buttons: 0,
+            wheel: 0,
+            pan: 0,
+        };
+        push_mouse_movement(rep_down).ok().unwrap_or(0);
+
         delay.delay_ms(500);
     }
+}
+
+fn push_mouse_movement(report: MouseReport) -> Result<usize, usb_device::UsbError> {
+    critical_section::with(|_| unsafe { USB_HID.as_mut().map(|hid| hid.push_input(&report)) })
+        .unwrap()
+}
+
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    // Handle USB request
+    let usb_dev = USB_DEVICE.as_mut().unwrap();
+    let usb_hid = USB_HID.as_mut().unwrap();
+    usb_dev.poll(&mut [usb_hid]);
 }
 
 // End of file
